089edfe560a062ccfe8b9dc202cafa51c8b69e5b
Gabriel Carrillo <gabrielcarrillo123@gmail.com>
2024-06-12 20:46:00 -0300

    Se descomentan test que no funcionaban

--- src/test/java/_US2/java/CA4.java
+++ src/test/java/_US2/java/CA4.java
@@ -1,28 +1,28 @@
-//package _US2.java;
-//
-//import java.util.Set;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import _SteGraMageCore.Discover;
-//import resources.Assert;
-//
-//class CA4 {
-//
-//	@Test
-//	void oneInterpreterTest() {
-//		Discover dis = new Discover();
-//		
-//		Set<Class<?>> result = null;
-//		try {
-//			result = dis.findClasses("plugins/codec");
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		
-//		String[] expected = {"ROT13"};
-//		
-//		Assert.equals(expected, result);
-//	}
-//
-//}
+package _US2.java;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import _SteGraMageCore.Discover;
+import resources.Assert;
+
+class CA4 {
+
+	@Test
+	void oneInterpreterTest() {
+		Discover dis = new Discover();
+		
+		Set<Class<?>> result = null;
+		try {
+			result = dis.findClasses("plugins/codec");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		String[] expected = {"ROT13"};
+		
+		Assert.equals(expected, result);
+	}
+
+}
